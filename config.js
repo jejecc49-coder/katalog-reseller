f97fee533ab3ca3482abc2ec9a3038cd2a975518
jejecc49-coder <jejecc49@gmail.com>
2026-06-26 22:11:54 +0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,5 @@
+window.CATALOG_CONFIG = {
+  sheetCsvUrl: "https://docs.google.com/spreadsheets/d/e/2PACX-1vT_8C1vsp0-il5ypObb5-bb-rlwY-De_t9AJTP0S0RUb8AevP6qRj7wWaURioUPrS2EKfLrOkzE5zb0/pub?output=csv",
+  whatsappNumber: "6281234567890",
+  storeName: "Katalog Reseller Pullboystore",
+};
